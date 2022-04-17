eeac77f580b2e570d8b35acde62fd80b75db1f8c
omarfarouk97 <omargaafar12@gmail.com>
2022-04-18 01:44:43 +0200

    second commit2

--- util/location.js
+++ util/location.js
@@ -1,4 +1,4 @@
-const APIKEY = "AIzaSyAPmYm-LA98n3yO0MTQpWW8DgkKM09_ow8"; //deleted
+const APIKEY = "your API_KEY"; //deleted
 const axios = require("axios");
 
 const HttpError = require("../models/http-error");
